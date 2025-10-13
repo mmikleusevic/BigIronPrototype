a08ebafb86cdc22932a59949e4e4aefd586a0f1e
mmikleusevic <mikleusevicmarin@gmail.com>
2025-10-13 10:47:22 +0200

    Added addressables with boot scene to loader scene loading

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Managers
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public static SoundManager Instance { get; private set; }
+        
+        public float SfxVolume { get; set; }
+        
+        [SerializeField] private AudioSource musicSource;
+        [SerializeField] private float transitionTime;
+        
+        private void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void SetVolume(float volume)
+        {
+            musicSource.volume = volume;   
+        }
+
+        public IEnumerator SmoothChangeTrack(AudioClip clip)
+        {
+            float startingVolume = musicSource.volume;
+            float endingVolume = 0;
+            
+            yield return StartCoroutine(SmoothChangeVolume(startingVolume, endingVolume));
+            
+            musicSource.clip = clip;
+            
+            float tempVolume = startingVolume;
+            startingVolume = musicSource.volume;
+            endingVolume = tempVolume;
+            
+            yield return StartCoroutine(SmoothChangeVolume(startingVolume, endingVolume));
+        }
+
+        private IEnumerator SmoothChangeVolume(float startingVolume, float endingVolume)
+        {
+            float timer = 0;
+            
+            while (timer < transitionTime)
+            {
+                float elapsed = timer / transitionTime;
+                
+                musicSource.volume = Mathf.Lerp(startingVolume, endingVolume, elapsed);
+                
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            
+            musicSource.volume = endingVolume;
+        }
+    }
+}
